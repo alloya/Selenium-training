812ff19a8a9651591aac42989fbc11d5ee9aee88
Yulia Lushinskaya <ylu@portia.ru>
2017-11-28 21:50:15 +0300

    FirstTest

---
+++ Training/Training/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Training")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Training")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b7e5350-a270-4354-b49b-f8507384f35d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Training/Training/Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace Training
+{
+	[TestFixture]
+	public class Test
+	{
+		private IWebDriver driver;
+
+		[SetUp]
+		public void SetUp()
+		{
+			driver = new ChromeDriver();
+		}
+
+		[Test]
+		public void NewTest()
+		{
+			driver.Navigate().GoToUrl("http://google.com");
+			driver.FindElement(By.Id("lst-ib")).SendKeys("Selenium");
+		}
+
+		[TearDown]
+		public void Dispose()
+		{
+			driver.Quit();
+		}
+	}
+}
