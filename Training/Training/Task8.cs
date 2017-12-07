426303a737d77a8396c7c05a7dddbea5578d9d01
Yulia Lushinskaya <ylu@portia.ru>
2017-12-07 13:09:07 +0300

    Task8

---
+++ Training/Training/Task8.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace Training
+{
+	public class Task8 : Base
+	{
+		[Test]
+		public void Task_8()
+		{
+			driver.Navigate().GoToUrl("http://localhost/litecart/en/");
+
+			var products = driver.FindElements(By.CssSelector(".product"));
+			Console.WriteLine(products.Count);
+
+			foreach (var item in products)
+			{
+				var sticker = item.FindElements(By.CssSelector(".sticker"));
+				
+				Assert.True(sticker.Count == 1);
+				Assert.True(sticker[0].Displayed);
+			}
+		}
+	}
+}
